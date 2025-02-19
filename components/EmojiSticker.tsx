903f0a76782f53452c552bc8b97b63db31547206
celestinebuendia <celestinembuendia@yahoo.com>
2025-02-18 19:18:30 -0800

    Demo part 6

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -10,6 +10,8 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
   const imageStyle = useAnimatedStyle(() => {
     return {
@@ -17,6 +19,19 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
       height: withSpring(scaleImage.value),
     }
   });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        }
+      ]
+    }
+  })
 
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
@@ -28,14 +43,21 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
       }
     });
 
+  const drag = Gesture.Pan().onChange(event => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image 
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]} />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image 
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]} />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
